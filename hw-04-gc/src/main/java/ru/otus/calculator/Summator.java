e825bbd1ceeb884686961c40ec450d2bf4ffbd99
Старицын Денис Юрьевич <Popcorn2008@mail.ru>
2024-12-01 13:23:06 +0700

    [hw-04-gc] optimization code

---
+++ hw-04-gc/src/main/java/ru/otus/calculator/Summator.java
@@ -0,0 +1,39 @@
+package ru.otus.calculator;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Getter
+public class Summator {
+    private int sum;
+    private int prevValue;
+    private int prevPrevValue;
+    private int sumLastThreeValues;
+    private int someValue;
+    // !!! эта коллекция должна остаться. Заменять ее на счетчик нельзя.
+    private final List<Data> listValues = new ArrayList<>();
+    private final Random random = new Random(10);
+
+    // !!! сигнатуру метода менять нельзя
+    public void calc(Data data) {
+        listValues.add(data);
+        if (listValues.size() % 100_000 == 0) {
+            listValues.clear();
+        }
+
+        sum += data.getValue() + random.nextInt();
+
+        sumLastThreeValues = data.getValue() + prevValue + prevPrevValue;
+
+        prevPrevValue = prevValue;
+        prevValue = data.getValue();
+
+        for (var idx = 0; idx < 3; idx++) {
+            someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
+            someValue = Math.abs(someValue) + listValues.size();
+        }
+    }
+}
